cb3072116cb3b7edc51a77e1f37b46eb4351e35d
shakilkhan1414 <khan.shakil.1414@gmail.com>
2023-04-16 02:25:03 +0600

    minor bug fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
     <header class="container-fluid text-center text-light p-3 bg-danger">
-        <div class="d-flex align-items-center justify-content-center">
-            <img src="@/assets/covid-logo.png" alt="covid19" width="80">
-            <h2 class="mx-3">Covid 19 Tracker</h2>  
-        </div>
+        <a href="/">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="@/assets/covid-logo.png" alt="covid19" width="80">
+                <h2 class="mx-3 text-light">Covid 19 Tracker</h2>
+            </div>
+        </a> 
     </header>  
 </template>
 
